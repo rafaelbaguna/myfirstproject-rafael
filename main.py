65cc1324d57c4bb2251e7f12239e5f969e433784
rafaelbaguna <131770685+rafaelbaguna@users.noreply.github.com>
2023-05-19 18:24:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-meme_dict = {
-            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
-            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
-            }
+import discord
 
-word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+intents = discord.Intents.default()
+intents.message_content = True
 
-if word in meme_dict.keys():
-    print(meme_dict[word])
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send(f'Hi! I am a bot {client.user}!')
+    elif  message.content.startswith('$heh'):
+        if len(message.content) > 4:
+            count_heh = int(message.content[4:])
+        else:
+            count_heh = 5
+        await message.channel.send("he" * count_heh)
+        
+client.run("MTEwMzk4MTc3NTYxMDQ1ODE5Mw.GllHbx.2W0wEDvGWfMs52B5g6gEwez0SQD1srjBMbd6rM")
